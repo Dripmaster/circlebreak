35aca39a8137f8ae3745772d56cad00bc627bd42
Dripmaster <son342@gmail.com>
2021-08-22 00:58:57 +0900

    buttonBase, sampleMap

--- Assets/Scripts/blockSpawner.cs
+++ Assets/Scripts/blockSpawner.cs
@@ -209,6 +209,13 @@ public class blockSpawner : MonoBehaviour
         }
         return g;
     }
+    public GameObject generateObject(GameObject obj)
+    {
+        var g = Instantiate(obj);
+        g.transform.parent = transform.parent;
+        g.transform.localPosition = Vector2.zero;
+        return g;
+    }
     public void SpawnBlocks()
     {
         float RangeMin = p.rangeMin;
@@ -274,7 +281,21 @@ public class blockSpawner : MonoBehaviour
             float theta = UnityEngine.Random.Range(0, passiveDangerZoneDegree * Mathf.Deg2Rad);
         theta = p.getTheta()+Mathf.PI +theta- passiveDangerZoneDegree * Mathf.Deg2Rad/2;
             g.GetComponent<blockBase>().setDest(theta, RangeMin + RangeLevel * RangeIdx, TimeOfSpawn, p);
+    }
+    public GameObject SpawnButton(GameObject obj)
+    {
+        float RangeMin = p.rangeMin;
+        float RangeMax = p.rangeMax;
+        float RangeMinToMax = RangeMax - RangeMin;
+        float RangeLevel = RangeMinToMax / RangeDivCount;
 
+        GameObject g = generateObject(obj); ;
+        int RangeIdx = 0;
+        RangeIdx = UnityEngine.Random.Range(0, RangeDivCount);
+        float theta = UnityEngine.Random.Range(0, passiveDangerZoneDegree * Mathf.Deg2Rad);
+        theta = p.getTheta() + Mathf.PI + theta - passiveDangerZoneDegree * Mathf.Deg2Rad / 2;
+        g.GetComponent<blockBase>().setDest(theta, RangeMin + RangeLevel * RangeIdx, TimeOfSpawn, p);
+        return g;
     }
     bool[] usedRange;
     int rangeCount;
